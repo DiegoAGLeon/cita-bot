ec166c708d072813159f9b82b7c506369eb4f2e3
Diego Gil <diego.89andres@gmail.com>
2021-12-15 20:40:19 +0100

    first commit

--- example1.py
+++ example1.py
@@ -8,12 +8,13 @@ if __name__ == "__main__":
         anticaptcha_api_key="... your key here ...",  # Anti-captcha API Key (auto_captcha=False to disable it)
         auto_captcha=False,  # Enable anti-captcha plugin (if False, you have to solve reCaptcha manually and press ENTER in the Terminal)
         auto_office=True,
-        chrome_driver_path="/usr/local/bin/chromedriver",
+        chrome_driver_path="C:/Users/Usuario/AppData/Local/Programs/Python/Python38/chromedriver",
         save_artifacts=True,  # Record available offices / take available slots screenshot
-        province=Province.BARCELONA,
-        operation_code=OperationType.RECOGIDA_DE_TARJETA,
-        doc_type=DocType.NIE,  # DocType.NIE or DocType.PASSPORT
-        doc_value="T1111111R",  # NIE or Passport number, no spaces.
+        province=Province.MADRID,
+        operation_code=OperationType.SOLICITUD_ASILO,
+        doc_type=DocType.PASSPORT,  # DocType.NIE or DocType.PASSPORT
+        doc_value="1111111",  # NIE or Passport number, no spaces.
+        year_of_birth = "2000",
         country="RUSIA",
         name="BORIS JOHNSON",  # Your Name
         phone="600000000",  # Phone number (use this format, please)
@@ -21,7 +22,7 @@ if __name__ == "__main__":
         # Offices in order of preference
         # This selects specified offices one by one or a random one if not found.
         # For recogida only the first specified office will be attempted or none
-        offices=[Office.BARCELONA_MALLORCA],
+        offices=[],
     )
     if "--autofill" not in sys.argv:
         try_cita(context=customer, cycles=200)  # Try 200 times
